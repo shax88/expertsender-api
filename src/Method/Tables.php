52ef23d49b5a8e6c5bf4abb10873a86977a4660c
Bartek Tyranowski <bartek.tyranowski@picodi.com>
2016-04-15 22:36:53 +0200

    Implemented the rest of Table methods

--- src/Method/Tables.php
+++ src/Method/Tables.php
@@ -21,101 +21,74 @@ class Tables extends AbstractMethod
     const ORDERBY_DIRECTION_DESC = 'Descending';
 
 
-//    /**
-//     * Add a row to table
-//     * @param $tableName
-//     * @param $rowData
-//     * @return bool
-//     */
-//    public function addRow($tableName, $rowData)
-//    {
-//        $result = $this->connection->post('DataTablesAddRow', $this->getRowXml($tableName, $rowData));
-//        return $result['code'] === 204;
-//    }
+    /**
+     * clears given data table
+     * @param $tableName
+     * @return bool
+     * @throws InvalidExpertsenderApiRequestException
+     * @throws MethodInMapperNotFoundException
+     */
+    public function clearTable($tableName)
+    {
+        $requestUrl = $this->buildApiUrl(self::METHOD_DataTablesClearTable);
+        $requestBody = $this->render('Tables/DataTablesClearTable', [
+            'tableName' => $tableName,
+            'apiKey' => $this->connection->getKey(),
+        ]);
+
+        $response = $this->connection->post($requestUrl, $requestBody);
+
+        $this->connection->isResponseValid($response);
+
+        return (boolean)$response->getBody();
+    }
 
     /**
-     * Delete row from datble
+     * Delete row from data table
      * @param $tableName
-     * @param array $rowData
+     * @param array $conditions
      * @return bool
      */
-    public function deleteRow($tableName, $rowData)
+    public function deleteRow($tableName, Array $conditions)
     {
-        $xml = $this->connection->getDefaultRequestXml();
-        $xml->addChild('TableName', $tableName);
-        $pkColumns = $xml->addChild('PrimaryKeyColumns');
-        foreach ($rowData as $columnName => $columnValue) {
-            $columnXml = $pkColumns->addChild('Column');
-            $columnXml->addChild('Name', $columnName);
-            $columnXml->addChild('Value', $columnValue);
-        }
-        $result = $this->connection->post('DataTablesDeleteRow', $xml);
-        return $result['code'] === 201;
+        $requestUrl = $this->buildApiUrl(self::METHOD_DataTablesDeleteRow);
+        $requestBody = $this->render('Tables/DataTablesDeleteRow', array_merge(['conditions' => $conditions], [
+            'tableName' => $tableName,
+            'apiKey' => $this->connection->getKey(),
+        ]));
+
+        $response = $this->connection->post($requestUrl, $requestBody);
+
+        $this->connection->isResponseValid($response);
+
+        return (boolean)$response->getBody();
     }
+
 
     /**
-     * Find rows by condition
+     * Updates single row in data table
      * @param $tableName
-     * @param array $selectColumns columns in result, ex. ['email', 'dog', 'age']
-     * @param array $conditions , ex.:
-     *  [
-     *      ['ColumnName' =>'email', 'Operator' => 'Equals', 'Value' => 'test@text.test'],
-     *      ['ColumnName' => 'dog', 'Operator' => 'Equals', 'Value' => 'beagle']
-     *  ]
-     * @return array
-     * @throws Exception
+     * @param array $conditions
+     * @return bool
      */
-    public function getRows($tableName, $selectColumns, Array $conditions = [])
+    public function updateRow($tableName, Array $data, Array $conditions)
     {
-        $xml = $this->connection->getDefaultRequestXml();
-        $xml->addChild('TableName', $tableName);
-        $columnsXml = $xml->addChild('Columns');
-        foreach ($selectColumns as $columnName) {
-            $columnsXml->addChild('Column', $columnName);
-        }
-        if (!empty($conditions)) {
-            $conditionXml = $xml->addChild('WhereConditions');
-            foreach ($conditions as $where) {
-                $whereXml = $conditionXml->addChild('Where');
-                $whereXml->addChild('ColumnName', $where['ColumnName']);
-                $whereXml->addChild('Operator', $where['Operator']);
-                $whereXml->addChild('Value', $where['Value']);
-            }
-        }
+        $requestUrl = $this->buildApiUrl(self::METHOD_DataTablesUpdateRow);
+        $requestBody = $this->render('Tables/DataTablesUpdateRow', array_merge(
+            ['conditions' => $conditions],
+            ['data' => $data],
+            [
+                'tableName' => $tableName,
+                'apiKey' => $this->connection->getKey(),
+            ]
+        ));
 
-        $result = $this->connection->post('DataTablesGetData', $xml, false);
+        $response = $this->connection->post($requestUrl, $requestBody);
 
-        if (!$result['code'] === 200) {
-            throw new Exception('Tables row request error');
-        }
-        $lines = explode(PHP_EOL, trim($result['response']));
-        unset($lines[0]);
-        $data = [];
-        foreach ($lines as $line) {
-            $data[] = str_getcsv($line, ',', "\r\n");
-        }
-        return $data;
+        $this->connection->isResponseValid($response);
+
+        return (boolean)$response->getBody();
     }
-
-    protected function getRowXml($tableName, $rowData)
-    {
-        $xml = $this->connection->getDefaultRequestXml();
-        $xml->addChild('TableName', $tableName);
-
-        $dataXml = $xml->addChild('Data');
-        $columnsXml = $dataXml->addChild('Columns');
-
-        foreach ($rowData as $columnName => $columnValue) {
-            $columnXml = $columnsXml->addChild('Column');
-            $columnXml->addChild('Name', $columnName);
-            $columnXml->addChild('Value', $columnValue);
-        }
-
-        return $xml;
-    }
-
-
-    // ------------------------------- PicodiLab
 
     /**
      * performs DataTablesGetData Api request
@@ -125,7 +98,7 @@ class Tables extends AbstractMethod
      * @return mixed
      * @throws InvalidExpertsenderApiRequestException
      */
-    public function doDataTablesGetData($tableName, Array $params = [], $format = 'csv')
+    public function getData($tableName, Array $params = [], $format = 'csv')
     {
         $defaultParams = [
             'Columns' => [],
@@ -149,6 +122,34 @@ class Tables extends AbstractMethod
         return $response->getBody();
     }
 
+
+    /**
+     * performs DataTablesGetDataCount - counts data table rows
+     * @param $tableName
+     * @param array $params
+     * @param string $format
+     * @return mixed
+     * @throws InvalidExpertsenderApiRequestException
+     */
+    public function getDataCount($tableName, Array $conditions = [])
+    {
+
+        $requestUrl = $this->buildApiUrl(self::METHOD_DataTablesGetDataCount);
+        $requestBody = $this->render('Tables/DataTablesGetDataCount', array_merge(['conditions' => $conditions], [
+            'tableName' => $tableName,
+            'apiKey' => $this->connection->getKey(),
+        ]));
+
+        $response = $this->connection->post($requestUrl, $requestBody);
+
+        $this->connection->isResponseValid($response);
+
+        $rXml = $this->connection->prepareResponse($response);
+        $cnt = (int)$rXml->xpath('//Count')[0];
+
+        return $cnt;
+    }
+
     /**
      * adds single row to data table
      * @param $tableName
@@ -170,8 +171,26 @@ class Tables extends AbstractMethod
         return (boolean)$ok;
     }
 
-    public function doDataTablesAddRowMultiple($tableName, Array $params)
+    /**
+     * adds multiple rows to data table
+     * @param $tableName
+     * @param array $rows
+     * @return bool
+     * @throws InvalidExpertsenderApiRequestException
+     * @throws MethodInMapperNotFoundException
+     */
+    public function addRows($tableName, Array $rows)
     {
+        $requestUrl = $this->buildApiUrl(self::METHOD_DataTablesAddRow);
+        $requestBody = $this->render('Tables/DataTablesAddRowMultiData', array_merge(['MultiData' => $rows], [
+            'tableName' => $tableName,
+            'apiKey' => $this->connection->getKey(),
+        ]));
 
+        $response = $this->connection->post($requestUrl, $requestBody);
+
+        $ok = $this->connection->isResponseValid($response);
+
+        return (boolean)$ok;
     }
 }
